29cebe1c5ad946bb1e0365718f3d815587317998
wisheverybodyhapi <wishhapi@163.com>
2025-05-18 16:40:33 +0800

    some updates

--- jupyter/qwen/qwen_project.py
+++ jupyter/qwen/qwen_project.py
@@ -5,7 +5,3 @@ cache_dir = "/raid/gfc/llm/models/"
 device = "cuda:7" if torch.cuda.is_available() else "cpu"
 tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-7B-Instruct", cache_dir=cache_dir)
 model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-7B-Instruct", cache_dir=cache_dir).to(device)
-
-print(tokenizer)
-print('-' * 88)
-print(model)
